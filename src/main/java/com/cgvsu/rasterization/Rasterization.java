3695d743f65843df77bcb0b278e8c67b228b350a
idashbox <dmerkusina@gmail.com>
2023-10-09 20:29:57 +0300

    добавлены пояснения к новому методу

--- src/main/java/com/cgvsu/rasterization/Rasterization.java
+++ src/main/java/com/cgvsu/rasterization/Rasterization.java
@@ -45,18 +45,22 @@ public class Rasterization {
         int centerX = width / 2;
         int centerY = height / 2;
 
+        // Преобразуем углы в радианы
         double startAngleRad = Math.toRadians(0);
         double endAngleRad = Math.toRadians(360);
 
+        // Рисуем окружность
         for (double indent = 0; indent <= radius; indent += 0.1) {
             final PixelWriter pixelWriter = gc.getPixelWriter();
             for (double angle = startAngleRad; angle <= endAngleRad; angle += 0.001) {
                 int x = (int) (centerX + (radius-indent) * Math.cos(angle));
                 int y = (int) (centerY + (radius-indent) * Math.sin(angle));
 
+                // Интерполируем цвет
                 double t = indent / radius;
                 Color interpolatedColor = interpolateColor(startColor, endColor, t);
 
+                // Рисуем пиксель
                 pixelWriter.setColor(x, y, interpolatedColor);
             }
         }
